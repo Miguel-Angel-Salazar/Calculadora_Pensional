9248f7052cd0a392ecf29fbbd030d3edfe8befd2
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-19 21:57:07 -0500

    creacion de la segunda prueba

--- pylogic.py
+++ pylogic.py
@@ -4,8 +4,10 @@ def suma(lista: list[int], idx = 0):
     
     return lista[idx] + suma(lista, idx + 1)
 
-def promedio(lista: list[int]):
+def pension_total(lista: list[int]):
     if not lista:
         return 0
     
-    return suma(lista) / len(lista)
+    pension = suma(lista) / len(lista) * 0.65
+    return pension
+
--- test_1.py
+++ test_1.py
@@ -7,16 +7,29 @@ class CalculatorTest(unittest.TestCase):
     def test_normal1(self):
 
         #lista con los salarios de los ultimos 10 años
-        lista = [1,2,3,4,5,6,7,8,9,10]
+        lista = [4000000, 3100000, 3200000, 3300000, 3400000, 3500000, 3600000, 3700000, 3800000, 3900000]
 
         #Datos de salida
-        expected = 5.5
+        expected = 2307500
 
         #proceso
-        result = pylogic.promedio(lista)
+        result = pylogic.pension_total(lista)
 
         #Comprobacion
         self.assertAlmostEqual(expected, result, 2)
+    
+
+    def test_normal2(self):
+        #lista con los salarios de los ultimos 10 años
+        lista = [5000000, 2400000, 2450000, 3150000, 3800000, 4100000, 4565000, 4825000, 4825000, 4985000]
+
+        #Datos de salida
+        expected = 2606500
+
+        result = pylogic.pension_total(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
+        
 
 
 if __name__ == "__main__":
