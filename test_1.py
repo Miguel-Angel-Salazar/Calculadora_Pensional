656ce43f7b3cedeef6151b9188dce974c2075209
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-19 22:52:30 -0500

    cambios realizados con exito

--- test_1.py
+++ test_1.py
@@ -8,12 +8,16 @@ class CalculatorTest(unittest.TestCase):
 
         #lista con los salarios de los ultimos 10 años
         lista = [4000000, 3100000, 3200000, 3300000, 3400000, 3500000, 3600000, 3700000, 3800000, 3900000]
+        genero = "femenino"
+        edad = 57
+        semanas = 1000
+        num_hijos = 0
 
         #Datos de salida
         expected = 2307500
 
         #proceso
-        result = pylogic.pension_total(lista)
+        result = pylogic.pension_total(lista, genero, edad, semanas, num_hijos)
 
         #Comprobacion
         self.assertAlmostEqual(expected, result, 2)
@@ -22,11 +26,16 @@ class CalculatorTest(unittest.TestCase):
     def test_normal2(self):
         #lista con los salarios de los ultimos 10 años
         lista = [5000000, 2400000, 2450000, 3150000, 3800000, 4100000, 4565000, 4825000, 4825000, 4985000]
+        genero = "femenino"
+        edad = 59
+        semanas = 1000
+        num_hijos = 0
 
         #Datos de salida
         expected = 2606500
 
-        result = pylogic.pension_total(lista)
+        #proceso
+        result = pylogic.pension_total(lista, genero, edad, semanas, num_hijos)
 
         self.assertAlmostEqual(expected, result, 2)
         
