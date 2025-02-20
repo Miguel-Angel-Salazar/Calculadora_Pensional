f9ee7675d8380b1d12a930cdd50636e0a0a1f234
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-19 22:38:03 -0500

    metodo pension completado con algunas condiciones

--- pylogic.py
+++ pylogic.py
@@ -4,10 +4,28 @@ def suma(lista: list[int], idx = 0):
     
     return lista[idx] + suma(lista, idx + 1)
 
-def pension_total(lista: list[int]):
+def pension_total(lista: list[int], genero: str, edad: int, semanas: int, num_hijos: int):
     if not lista:
         return 0
     
     pension = suma(lista) / len(lista) * 0.65
-    return pension
+
+    if (genero == "masculino" and edad >= 62 and semanas >= 1300):
+        if pension < 1423500:
+            return 1423500
+        else:
+            return pension
+    
+    elif (genero == "femenino" and edad >= 57):
+        if num_hijos > 3:
+            num_hijos = 3
+        
+        cuenta_semanas = semanas - (50 * num_hijos)
+
+        if (cuenta_semanas >= semanas):
+            return pension
+
+    else:
+        return "No cumple con todos los requisitos"
+    
 
