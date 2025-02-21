52ec6119c7ff6e56423d81beed998d1fa1cd4e07
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-21 09:47:33 -0500

    inicio de la consola

--- console.py
+++ console.py
@@ -1,3 +1,12 @@
 #buenas tardes queridos senadores de la republica les comento que aca van todo lo que se le muestra al usuario en pantalla como bien dice frontend
+print("\n          Bienvenidos a la calculadora \n--------------------------------------------------\n\nPorfavor ingresa tu salario de los ultimos 10 años\n")
+
+lista = []
+for i in range(1, 11, 1):
+    i = int(input(f"Ingrese su {i} salario: "))
+    lista.append(i)
+
+
+print(lista)
 
 
