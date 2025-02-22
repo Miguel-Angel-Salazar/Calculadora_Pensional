f46577a8589b1d5c5d25b891e37700a0f570509e
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-22 14:04:38 -0500

    Creacion de la consola completado

--- console.py
+++ console.py
@@ -1,3 +1,5 @@
+import pylogic
+
 #Metodos
 def gender_asigned(valor):
     match valor:
@@ -7,17 +9,22 @@ def gender_asigned(valor):
         case (2):
             return "Femenino"
 
-
-
-#Comunicacion y creacion de variables
-print("\n          Bienvenidos a la calculadora \n--------------------------------------------------\n\nPorfavor ingresa tu salario de los ultimos 10 años\n")
-
+#Creacion de variables
 lista = []
 genero = ""
+edad = 0
+semanas = 0
+
+#Comunicacion con el usuario
+print("\n          Bienvenidos a la calculadora \n--------------------------------------------------\n\nPorfavor ingresa tu salario de los ultimos 10 años\n")
+
 
 gen_select = int(input(f"Por favor selecciona tu genero: \n\n 1. Masculino \n 2. Femenino \n\nSeleccion: "))
 genero = gender_asigned(gen_select)
 
+edad = int(input("Ingresa tu edad actual: "))
+semanas = int(input("ingrese el total de semanas cotizadas: "))
+num_hijos = int(input("Cuantos Hijos tienes: "))
 
 for i in range(1, 11, 1):
     i = int(input(f"Ingrese su {i} salario: "))
@@ -26,4 +33,7 @@ for i in range(1, 11, 1):
 
 print(lista)
 
+
+#Comunicacion con la logica
+print(pylogic.pension_total(lista, genero, edad, semanas, num_hijos))
 
