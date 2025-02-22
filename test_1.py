2cd323fa12ad9880e3b7edf4c32590cb5f1f2d46
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-22 14:19:02 -0500

    Pruebas completadas

--- test_1.py
+++ test_1.py
@@ -156,16 +156,70 @@ class Pension_Total_Test(unittest.TestCase):
 
 class Calculadora_ibl_Test(unittest.TestCase):
     
-    def test_proN1(self):
+    def test_IBL_Nor1(self):
         #Entradas
         lista = [4000000, 3100000, 3200000, 3300000, 3400000, 3500000, 3600000, 3700000, 3800000, 3900000]
 
-        expected = 3550000
+        expected = 35500000
 
         result = pylogic.calculo_IBL(lista)
 
         self.assertAlmostEqual(expected, result, 2)
 
+    
+    def test_IBL_Nor2(self):
+        #Entradas
+        lista = [5000000, 2400000, 2450000, 3150000, 3800000, 4100000, 4565000, 4825000, 4825000, 4985000]
+
+        expected = 40100000
+
+        result = pylogic.calculo_IBL(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
+    
+
+    def test_IBL_Nor3(self):
+        #Entradas
+        lista = [10235685, 7100000, 7425690, 7956124, 8000000, 8245687, 8564752, 8900000, 9300000, 10000000]
+
+        expected = 85727938
+
+        result = pylogic.calculo_IBL(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
+    
+    
+    def test_IBL_Extra1(self):
+        #Entradas
+        lista = [1300000, 689455, 737717, 781242, 828116, 877803, 908526, 1000000, 1160000, 1300000]
+
+        expected = 9582859
+
+        result = pylogic.calculo_IBL(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
+
+
+    def test_IBL_Extra2(self):
+        #Entradas
+        lista = [3900000, 1569875, 1750000, 1800000, 2000000, 2356987, 2565450, 2900000, 3250000, 3498520]
+
+        expected = 25590832
+
+        result = pylogic.calculo_IBL(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
+
+    
+    def test_IBL_Extra3(self):
+        #Entradas
+        lista = [4000000, 2200000, 2400000, 2600000, 2800000, 2900000, 3000000, 5600000, 4100000, 4567898]
+
+        expected = 34167898
+
+        result = pylogic.calculo_IBL(lista)
+
+        self.assertAlmostEqual(expected, result, 2)
 
 
 if __name__ == "__main__":
